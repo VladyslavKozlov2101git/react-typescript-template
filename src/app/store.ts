87d288dece1bbd5544f33d9bad6e4652d476a5a8
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2023-06-01 21:51:06 +0300

    Store upd

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
+import axios from "axios";
+
+const authMiddleware: Middleware = () => (next) => (action) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+    } else {
+        delete axios.defaults.headers.common["Authorization"];
+    }
+
+    return axios.interceptors.response.use(
+        () => {
+            return next(action);
+        },
+        (error) => {
+            if (error.response && error.response.status === 401) {
+                localStorage.clear();
+            }
+            return Promise.reject(error);
+        }
+    );
+};
+
 export const setupStore = () => {
     return configureStore({
-        reducer: rootReducer
-    })
-}
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(authMiddleware),
+    });
+};
+
 export const store = setupStore();
 
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore["dispatch"]
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
